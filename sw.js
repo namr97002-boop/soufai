6381820de9bc58485544c38df68f76e516593870
namr97002-boop <namr97002@gmail.com>
2026-08-06 23:13:41 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // نظام مياه السوفعي - Service Worker المثالي
 // ==========================================
 
-const CACHE_NAME = 'soufai-v11';
+const CACHE_NAME = 'soufai-v12';
 
 
 const FILES_TO_CACHE = [
